5cecddd44277dd2f7cbc58aac4313b644054e48c
RohanAgarwal3302 <113240889+RohanAgarwal3302@users.noreply.github.com>
2022-09-12 18:38:48 +0530

    Add files via upload

---
+++ Palindrome Test/Palindrome test.py
@@ -0,0 +1,25 @@
+#Tests if the given string is a palindrome or not
+def palindrome_test(string):
+    if string==string[::-1]:
+        return True            
+    else:
+        return False
+run=True
+while run==True:
+    #Takes an input from the user
+    x=input("Enter a string to test for palindrome or 'exit':")
+    #Exits the program if the user inputs "exit"
+    if x=="exit":
+        print("Thank you for your time.")
+        run=False
+        break
+    else:
+        #Takes the string input and converts it into a alphanumeric set
+        x=x.lower()
+        test_set=  ""
+        for c in x:
+            if c.isalnum():
+                test_set += c
+        #Checks wether the given string is palindrome or not    
+        print("Is Palindrome:",palindrome_test(test_set))
+        
